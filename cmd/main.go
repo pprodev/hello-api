8cbf7128c7312b1432d9795e756737f0bb1e945e
Chris <8647194+pprodev@users.noreply.github.com>
2022-12-27 14:57:51 -0800

    added deployment to AppEngine

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"github.com/pprodev/hello-api/handlers"
 	"github.com/pprodev/hello-api/handlers/rest"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	addr := ":8080"
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if addr == ":" {
+		addr = ":8080"
+	}
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck)
 
 	log.Printf("listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
